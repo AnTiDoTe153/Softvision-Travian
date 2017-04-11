bf844fa9c4349940e7c28bf3c95b66242d2ddb68
Calin <calin.g.sulea@gmail.com>
2017-04-11 21:01:53 +0300

    saddasd

---
+++ Appl1/WebApplication1/Controllers/MineController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Authorize]
+    public class MineController : Controller
+    {
+        // GET: Mine
+        ApplicationDbContext dbContext = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+            var userId = this.User.Identity.GetUserId();
+            var user = dbContext.Users.Find(userId);
+            var city = user.Cities.First();
+            return View(city);
+        }
+
+        public ActionResult Details(int mineId)
+        {
+            var mine = dbContext.Mines.Find(mineId);
+            
+            return View(mine);
+        }
+    }
+}
---
+++ Appl1/WebApplication1/Models/City.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class City
+    {
+        public int CityId { get; set; }
+        public virtual IList<Mine> Mines { get; set; }
+        public virtual IList<Resource> Resources { get; set; }
+
+        public String ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+
+    public class Resource
+    {
+
+        public int ResourceId { get; set; }
+        public int CityId { get; set; }
+        public virtual City City { get; set; }
+
+        public DateTime LastUpdate { get; set; }
+        public ResourceType Type { get; set; }
+
+        public double Value { get; set; }
+    }
+
+    public class Mine
+    {
+        public int MineId { get; set; }
+        public virtual City City { get; set; }
+
+        public int Level { get; set; }
+
+        public ResourceType Type { get; set; }
+    }
+
+    public enum ResourceType
+    {
+        Wheat,
+        Iron,
+        Clay,
+        Wood
+    }
+}
